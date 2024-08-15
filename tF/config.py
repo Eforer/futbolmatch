fe741b2667f6c2a666fef171d0f9dac991a2339c
Eforer <E4e.1997@gmail.com>
2024-08-15 14:30:02 -0400

    fix

--- tF/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'supersecretkey'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost/futbol_app'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
